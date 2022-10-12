42bcb8b2266ba226200334b20007cbb242ee9049
MiraVV <94649037+MiraVV@users.noreply.github.com>
2022-10-12 22:59:58 +0000

    Deploying to gh-pages from @ MiraVV/goit-react-hw-05-movies@f60e904edf8016081df6df0dc25b0647af4777e5 🚀

---
+++ static/js/61.f72eb331.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[61],{191:function(e,n,r){r.r(n),r.d(n,{default:function(){return o}});var t=r(731),i=r(184);function o(){return(0,i.jsxs)("div",{children:[(0,i.jsx)("h1",{children:" 404 Sorry, the page was not found"}),(0,i.jsx)(t.rU,{to:"/",children:"To main page"})]})}}}]);
+//# sourceMappingURL=61.f72eb331.chunk.js.map
